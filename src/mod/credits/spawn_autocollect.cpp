5c3cd3f26d1e417cf6e015e4a385a5c12a75eec9
mtxfellen <21310017+mtxfellen@users.noreply.github.com>
2026-06-30 09:35:08 +0100

    Replace old autocollect with a patch for using BSP collision instead of AABB

--- src/mod/credits/spawn_autocollect.cpp
+++ src/mod/credits/spawn_autocollect.cpp
@@ -1,55 +1,39 @@
+#ifdef SE_IS_TF2
 #include "mod.h"
 #include "stub/nav.h"
 #include "stub/tfentities.h"
 #include "stub/gamerules.h"
-#include "stub/tf_objective_resource.h"
-#include "util/scope.h"
-
 
 namespace Mod::Credits::Spawn_AutoCollect
 {
-	/* which team is allowed to collect credits? */
-	int GetCreditTeamNum()
+	void AutoCollect(CCurrencyPack *pack)
 	{
-		/* HACK: make behavior consistent with another mod */
-		static ConVarRef sig_mvm_set_credit_team("sig_mvm_set_credit_team");
-		if (sig_mvm_set_credit_team.IsValid() && sig_mvm_set_credit_team.GetBool()) {
-			return sig_mvm_set_credit_team.GetInt();
-		}
-		
-		return TF_TEAM_RED;
+		if (!pack->IsDistributed())
+			TFGameRules()->DistributeCurrencyAmount(pack->GetAmount(), NULL, true, false, false);
+
+		pack->SetTouched(true);
+		UTIL_Remove(pack);
 	}
-	
-	
+
 	DETOUR_DECL_MEMBER(void, CCurrencyPack_ComeToRest)
 	{
+		auto item = reinterpret_cast<CItem *>(this);
+		item->ComeToRest(); // BaseClass::ComeToRest()
+
 		auto pack = reinterpret_cast<CCurrencyPack *>(this);
-		
-		DETOUR_MEMBER_CALL();
-		
-		if (!pack->IsMarkedForDeletion() && !pack->IsBeingPulled() && !pack->IsDistributed()) {
-			int credit_team = GetCreditTeamNum();
-			if (credit_team == TF_TEAM_RED || credit_team == TF_TEAM_BLUE) {
-				TFNavAttributeType spawn_attr = ((credit_team == TF_TEAM_RED) ? BLUE_SPAWN_ROOM : RED_SPAWN_ROOM);
-				
-				auto area = reinterpret_cast<CTFNavArea *>(TheNavMesh->GetNavArea(pack->GetAbsOrigin()));
-				if (area != nullptr && area->HasTFAttributes(spawn_attr)) {
-					DevMsg("Non-distributed CCurrencyPack (#%d / $%d) landed in spawn room nav area; distributing now.\n",
-						ENTINDEX(pack), pack->GetAmount());
-					
-					pack->SetDistributed(true);
-					
-					TFGameRules()->DistributeCurrencyAmount(pack->GetAmount(), nullptr, true, false, false);
-					TFObjectiveResource()->m_nMvMWorldMoney -= pack->GetAmount();
-					
-					StopParticleEffects(pack);
-					DispatchParticleEffect("mvm_cash_embers_red", PATTACH_ABSORIGIN_FOLLOW, pack);
-				}
-			}
+
+		if (pack->IsMarkedForDeletion() || pack->IsClaimed())
+			return;
+
+		if (TheNavMesh->GetNavArea(pack->GetAbsOrigin()) == NULL ||
+			IsTakingTriggerHurtDamageAtPoint(pack->GetAbsOrigin()) ||
+			PointInRespawnRoom(NULL, pack->GetAbsOrigin(), false))
+		{
+			AutoCollect(pack);
+			return;
 		}
 	}
-	
-	
+
 	class CMod : public IMod
 	{
 	public:
@@ -59,11 +43,11 @@ namespace Mod::Credits::Spawn_AutoCollect
 		}
 	};
 	CMod s_Mod;
-	
-	
-	ConVar cvar_enable("sig_credits_spawn_autocollect", "0", FCVAR_NOTIFY,
-		"Mod: auto-collect credits that land in the bots' spawn area",
+
+	ConVar cvar_enable("sig_credits_spawn_autocollect_fix", "0", FCVAR_NOTIFY,
+		"Mod: Fix credit autocollection by triggers using AABB collision instead of BSP.",
 		[](IConVar *pConVar, const char *pOldValue, float flOldValue){
 			s_Mod.Toggle(static_cast<ConVar *>(pConVar)->GetBool());
 		});
 }
+#endif
