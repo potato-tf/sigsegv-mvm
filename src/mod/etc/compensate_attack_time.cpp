f48e51f4f148ce2935c90de32ba92684636ea276
mtxfellen <21310017+mtxfellen@users.noreply.github.com>
2026-06-30 01:31:17 +0100

    Add Etc:Compensate_Attack_time

---
+++ src/mod/etc/compensate_attack_time.cpp
@@ -0,0 +1,56 @@
+#ifdef SE_IS_TF2
+#include "mod.h"
+#include "stub/tfweaponbase.h"
+#include "stub/objects.h"
+
+namespace Mod::Etc::Compensate_Attack_time
+{
+	DETOUR_DECL_MEMBER(void, CTFWeaponBaseGun_PrimaryAttack)
+	{
+		auto weapon = reinterpret_cast<CTFWeaponBaseGun *>(this);
+
+		float current_attack = weapon->m_flNextPrimaryAttack;
+		float delta_attack = gpGlobals->curtime - current_attack;
+
+		DETOUR_MEMBER_CALL();
+
+		float flFireDelay = weapon->m_flNextPrimaryAttack - gpGlobals->curtime;
+
+		if (delta_attack >= 0.f && delta_attack <= gpGlobals->interval_per_tick)
+			weapon->m_flNextPrimaryAttack = current_attack + flFireDelay;
+	}
+
+	DETOUR_DECL_MEMBER(void, CObjectSentrygun_Attack)
+	{
+		// Note that this does not raise the maximum fire rate on sentries.
+		auto sentry = reinterpret_cast<CObjectSentrygun *>(this);
+
+		float current_attack = sentry->m_flNextAttack;
+		float delta_attack = gpGlobals->curtime - current_attack;
+
+		DETOUR_MEMBER_CALL();
+
+		float fire_interval = sentry->m_flNextAttack - gpGlobals->curtime;
+
+		if (delta_attack >= 0.f && delta_attack <= gpGlobals->interval_per_tick)
+			sentry->m_flNextAttack = current_attack + fire_interval;
+	}
+
+	class CMod : public IMod
+	{
+	public:
+		CMod() : IMod("Etc:Compensate_Attack_time")
+		{
+			MOD_ADD_DETOUR_MEMBER(CTFWeaponBaseGun_PrimaryAttack, "CTFWeaponBaseGun::PrimaryAttack");
+			MOD_ADD_DETOUR_MEMBER(CObjectSentrygun_Attack, "CObjectSentrygun::Attack");
+		}
+	};
+	CMod s_Mod;
+
+	ConVar cvar_enable("sig_etc_compensate_attack_time", "0", FCVAR_NOTIFY,
+		"Etc: Modify the primary attack time interval to prevent incorrect rounding up of the attack interval.",
+		[](IConVar *pConVar, const char *pOldValue, float flOldValue){
+			s_Mod.Toggle(static_cast<ConVar *>(pConVar)->GetBool());
+		});
+}
+#endif
